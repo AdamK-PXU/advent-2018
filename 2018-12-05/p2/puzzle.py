1b84fdec4b98bf5ac23d623d10e404ae4b3f4e12
Adam Kirkland <adam.kirkland@pixelunion.net>
2018-12-04 22:48:56 -0800

    Optimization to 2018-12-05 part 2.

--- 2018-12-05/p2/puzzle.py
+++ 2018-12-05/p2/puzzle.py
@@ -5,7 +5,7 @@ filename = "input.txt"
 with open(filename) as f:
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     polymer = f.readlines()
-    originalPolymer = polymer[0]
+    originalPolymer = polymer[0].strip()
 
     for i in range(len(alphabet)):
         polymer = originalPolymer
@@ -17,32 +17,20 @@ with open(filename) as f:
                 adjustedPolymer += polymer[j]
 
         polymer = adjustedPolymer
+        reducedPolymer = []
 
-        while encounteredReaction:
-            reactedPolymer = ''
-            encounteredReaction = False
-
-            for j in range(len(polymer) - 1):
-                c1 = polymer[j]
-                c2 = polymer[j + 1]
-
-                if c1.upper() == c2.upper():
-                    if c1.isupper() and c2.isupper() != True:
-                        reactedPolymer += polymer[(j + 2):]
-                        encounteredReaction = True
-                        break
-                    elif c1.isupper() != True and c2.isupper():
-                        reactedPolymer += polymer[(j + 2):]
-                        encounteredReaction = True
-                        break
-                    else:
-                        reactedPolymer += c1
+        for c in polymer:
+            if len(reducedPolymer) > 0 and reducedPolymer[-1].upper() == c.upper():
+                if reducedPolymer[-1].isupper() and c.isupper() != True:
+                    reducedPolymer.pop()
+                elif reducedPolymer[-1].isupper() != True and c.isupper():
+                    reducedPolymer.pop()
                 else:
-                    reactedPolymer += c1
+                    reducedPolymer.append(c)
+            else:
+                reducedPolymer.append(c)
 
-            polymer = reactedPolymer
-
-        alphabet[i] = len(polymer)
+        alphabet[i] = len(reducedPolymer)
 
     shortestPolymerSize = sys.maxsize
     shortestPolymerIndex = -1
