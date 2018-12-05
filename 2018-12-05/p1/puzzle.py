f2c1fd2663dac28a2b31179da47101751e570e5a
Adam Kirkland <adam.kirkland@pixelunion.net>
2018-12-04 22:47:29 -0800

    Optimization to 2018-12-05 part 1.

--- 2018-12-05/p1/puzzle.py
+++ 2018-12-05/p1/puzzle.py
@@ -2,31 +2,18 @@ filename = "input.txt"
 
 with open(filename) as f:
     polymer = f.readlines()
-    polymer = polymer[0]
-    encounteredReaction = True
+    polymer = polymer[0].strip()
+    reducedPolymer = []
 
-    while encounteredReaction:
-        reactedPolymer = ''
-        encounteredReaction = False
-
-        for i in range(len(polymer) - 1):
-            c1 = polymer[i]
-            c2 = polymer[i + 1]
-
-            if c1.upper() == c2.upper():
-                if c1.isupper() and c2.isupper() != True:
-                    reactedPolymer += polymer[(i + 2):]
-                    encounteredReaction = True
-                    break
-                elif c1.isupper() != True and c2.isupper():
-                    reactedPolymer += polymer[(i + 2):]
-                    encounteredReaction = True
-                    break
-                else:
-                    reactedPolymer += c1
+    for c in polymer:
+        if len(reducedPolymer) > 0 and reducedPolymer[-1].upper() == c.upper():
+            if reducedPolymer[-1].isupper() and c.isupper() != True:
+                reducedPolymer.pop()
+            elif reducedPolymer[-1].isupper() != True and c.isupper():
+                reducedPolymer.pop()
             else:
-                reactedPolymer += c1
+                reducedPolymer.append(c)
+        else:
+            reducedPolymer.append(c)
 
-        polymer = reactedPolymer
-
-    print len(polymer)
+    print len(reducedPolymer)
